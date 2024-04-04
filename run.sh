7e30e966bad4a0de6cfb366c881b5e20563c9645
Fabien Bézagu <fbezagu@ippon.fr>
2024-04-04 15:03:24 +0200

    feat: run api with start script (called by run.sh)

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 if [ ! -d "localstack" ]; then
   echo "localstack not installed -> DOWNLOADING it"
   wget https://github.com/localstack/localstack-cli/releases/download/v3.3.0/localstack-cli-3.3.0-linux-amd64.tar.gz
@@ -5,4 +9,5 @@ if [ ! -d "localstack" ]; then
   rm localstack-cli-3.3.0-linux-amd64.tar.gz
 fi
 
-./localstack/localstack start &
+$SCRIPT_DIR/localstack/localstack start &
+$SCRIPT_DIR/tournament-api/start.sh
--- tournament-api/start.sh
+++ tournament-api/start.sh
@@ -1,1 +1,4 @@
-AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test ./gradlew run
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test $SCRIPT_DIR/gradlew -p $SCRIPT_DIR run
